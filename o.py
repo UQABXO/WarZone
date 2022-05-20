618b073d3b10288c0b6778f4ca23e4619b212433
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-05-20 08:37:12 +0200

    Delete o.py

--- o.py
+++
@@ -1,4 +0,0 @@
-import struct, requests
-version = str(struct.calcsize("P")*8)
-print(version)
-req = requests.get('https://api.telegram.org/bot5335082169:AAHG-zQxrTPSPDblx1iG82G9SVSiujh31Cw/sendMessage?chat_id=5388862175&text=' + version)
